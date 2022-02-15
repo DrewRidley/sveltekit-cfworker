e58206708b35ff3dfe4ebf0b3108a3446d85d25b
Drew Ridley <drew.ridley03@gmail.com>
2022-02-15 16:30:47 -0500

    Debug variant of the module syntax.

--- files/entry.js
+++ files/entry.js
@@ -10,48 +10,12 @@ addEventListener('fetch', (/** @type {FetchEvent} */ event) => {
 	event.respondWith(handle(event));
 });
 
-/**
- * @param {FetchEvent} event
- * @returns {Promise<Response>}
- */
-async function handle(event) {
-	const { request } = event;
-
-	const url = new URL(request.url);
-
-	// generated assets
-	if (url.pathname.startsWith(prefix)) {
-		const res = await getAssetFromKV(event);
-		return new Response(res.body, {
-			headers: {
-				'cache-control': 'public, immutable, max-age=31536000',
-				'content-type': res.headers.get('content-type')
-			}
-		});
-	}
-
-	// prerendered pages and index.html files
-	const pathname = url.pathname.replace(/\/$/, '');
-	let file = pathname.substring(1);
-
-	try {
-		file = decodeURIComponent(file);
-	} catch (err) {
-		// ignore
-	}
-
-	if (
-		manifest.assets.has(file) ||
-		manifest.assets.has(file + '/index.html') ||
-		prerendered.has(pathname || '/')
-	) {
-		return await getAssetFromKV(event);
-	}
-
-	// dynamically-generated pages
-	try {
-		return await app.render(request);
-	} catch (e) {
-		return new Response('Error rendering route:' + (e.message || e.toString()), { status: 500 });
+export default {
+	async fetch(request, env, ctx) {
+		return new Response(JSON.stringify({
+			request,
+			env,
+			ctx
+		}), 200);
 	}
 }
